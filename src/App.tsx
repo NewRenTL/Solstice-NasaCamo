2f91495e78034ade692666301c52af19aa563005
NewRenTL <diego.bustamante@utec.edu.pe>
2024-10-05 18:32:25 -0500

    fix(modules): I added react-router-dom to route with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold ">Starting!</h1>
+      <div className="flex flex-col w-full bg-slate-400">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element = {<Home></Home>} ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Home  = () => {
+  return (
+    <div className='flex flex-col w-full h-lvh'>
+        <h2>Hello World</h2>
+    </div>
+  )
+}
+
+export default Home
