b75d4aec0b137d1db620d9adfbab68f97ffc6020
NewRenTL <diego.bustamante@utec.edu.pe>
2024-10-06 14:02:32 -0500

    feat(popup) : pre pop-up

--- src/components/HomeComponents/CreationMicroCreature/TemplateCreation.tsx
+++ src/components/HomeComponents/CreationMicroCreature/TemplateCreation.tsx
@@ -1,7 +1,120 @@
 import { useState } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
+const Metales = [
+  { value: "HIERRO", label: "Hierro" },
+  { value: "NÍQUEL", label: "Niquel" },
+  { value: "MAGNESIO", label: "Magnesio" },
+  { value: "AZUFRE", label: "Azufre" },
+  { value: "COBRE", label: "Cobre" },
+  { value: "CLORUROS", label: "Cloruros" },
+  { value: "COBALTO", label: "Cobal" },
+  { value: "SULFATOS", label: "Sulfatos" },
+  { value: "CLORUROS", label: "Cloruros" },
+];
+
+const Compositions = [
+  { value: "AGUA", label: "Agua" },
+  { value: "SULFATOS", label: "Sulfatos" },
+  { value: "CLORUROS", label: "Cloruros" },
+  { value: "AMONÍACO", label: "Amoníaco" },
+  { value: "METANO", label: "Metano" },
+  { value: "HIERROLIQUIDO", label: "Hierro liquido" },
+];
+
+const EnergySources = [
+  {
+    value: "GEOTERMICA",
+    label: "Energia Geotermica",
+  },
+  {
+    value: "QUIMIOSÍNTESIS",
+    label: "Energia QuimioSíntesis",
+  },
+];
+
+const AtmosphereCompostions = [
+  {
+    value: "H20",
+    label: "Dioxido de Carbono",
+  },
+  {
+    value: "N2",
+    label: "Nitrogeno",
+  },
+  {
+    value:'CH4',
+    label:'Metano'
+  },
+  {
+    value:'NH3',
+    label:'Amoníaco'
+  },
+  {
+    value:'HE',
+    label:'Helio'
+  },
+  {
+    value:'H2',
+    label:'Hidrógeno'
+  },
+
+]
+
+const animatedComponents = makeAnimated();
 
 const TemplateCreation = () => {
   const [isPopUpVisible, setIsPopUpVisible] = useState(false);
+  const [sliderValue, setSliderValue] = useState(50);
+  const [sliderValue2, setSliderValue2] = useState(0);
+  const [sliderValue3, setSliderValue3] = useState(2);
+  const [selectedMetales, setSelectedMetales] = useState([]);
+  const [selectedComposition, setSelectedComposition] = useState([]);
+  const [selectedATMComposition, setSelectedATMComposition] = useState([]);
+
+  const [selectedEnergy, setSelectedEnergy] = useState(null);
+
+  // Manejar los cambios en las opciones seleccionadas
+  const handleSelectChangeMetales = (selected) => {
+    if (selected) {
+      // Guardar solo los valores seleccionados en el estado
+      const selectedValues = selected.map((option) => option.value);
+      console.log(selectedValues);
+      setSelectedMetales(selectedValues);
+    } else {
+      // Si no hay selección, resetear el array
+      setSelectedMetales([]);
+    }
+  };
+  const handleSelectChangeComposition = (selected) => {
+    if (selected) {
+      // Guardar solo los valores seleccionados en el estado
+      const selectedValues = selected.map((option) => option.value);
+      console.log(selectedValues);
+      setSelectedComposition(selectedValues);
+    } else {
+      // Si no hay selección, resetear el array
+      setSelectedComposition([]);
+    }
+  };
+  const handleSelectChangeATMComposition = (selected) => {
+    if (selected) {
+      // Guardar solo los valores seleccionados en el estado
+      const selectedValues = selected.map((option) => option.value);
+      console.log(selectedValues);
+      setSelectedATMComposition(selectedValues);
+    } else {
+      // Si no hay selección, resetear el array
+      setSelectedATMComposition([]);
+    }
+  };
+
+  const handleSelectChangeEnerygySource = (selected) => {
+    setSelectedEnergy(selected);
+  };
 
   // Function to show the pop-up
   const showPopUp = () => {
@@ -27,13 +140,151 @@ const TemplateCreation = () => {
 
       {isPopUpVisible && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-[80%] sm:w-[40%]">
+          <div className="bg-white p-6 rounded-lg overflow-y-auto h-[70%] shadow-lg w-[80%] sm:w-[40%]">
             <h2 className="text-xl font-bold mb-4">
               Enter data to create a creature:
             </h2>
-            <div className="flex flex-col mn:flex-row">
-              
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Temperatura:
+                </label>
+                <Slider
+                  min={-150}
+                  max={400}
+                  className="w-full"
+                  value={sliderValue}
+                  onChange={(value) => setSliderValue(value as number)}
+                  trackStyle={{ backgroundColor: "blue", height: 10 }}
+                  handleStyle={{
+                    borderColor: "blue",
+                    height: 20,
+                    width: 20,
+                    marginTop: -5,
+                    backgroundColor: "white",
+                  }}
+                  railStyle={{ backgroundColor: "gray", height: 10 }}
+                />
+                <p className="flex w-full my-2">
+                  Selected Value: {sliderValue}
+                </p>
+              </div>
             </div>
+
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Presión:
+                </label>
+                <Slider
+                  min={0.1}
+                  max={1000}
+                  className="w-full"
+                  value={sliderValue2}
+                  onChange={(value) => setSliderValue2(value as number)}
+                  trackStyle={{ backgroundColor: "blue", height: 10 }}
+                  handleStyle={{
+                    borderColor: "blue",
+                    height: 20,
+                    width: 20,
+                    marginTop: -5,
+                    backgroundColor: "white",
+                  }}
+                  railStyle={{ backgroundColor: "gray", height: 10 }}
+                />
+                <p className="flex w-full my-2">
+                  Selected Value: {sliderValue2}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Metales:
+                </label>
+                <Select
+                  isMulti
+                  options={Metales} // Lista de opciones
+                  onChange={handleSelectChangeMetales} // Manejador de cambios
+                  components={animatedComponents}
+                  classNamePrefix="select"
+                  placeholder="Choose options..." // Placeholder para las opciones
+                />
+              </div>
+            </div>
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Fuente de Energia:
+                </label>
+                <Select
+                  options={EnergySources} // Lista de opciones
+                  onChange={handleSelectChangeEnerygySource} // Manejador de cambios
+                  classNamePrefix="select"
+                  placeholder="Choose options..." // Placeholder para las opciones
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Composición del ambiente:
+                </label>
+                <Select
+                  isMulti
+                  options={Compositions} // Lista de opciones
+                  onChange={handleSelectChangeComposition} // Manejador de cambios
+                  components={animatedComponents}
+                  classNamePrefix="select"
+                  placeholder="Choose options..." // Placeholder para las opciones
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">
+                  Composición de la atmosphera:
+                </label>
+                <Select
+                  isMulti
+                  options={AtmosphereCompostions} // Lista de opciones
+                  onChange={handleSelectChangeATMComposition} // Manejador de cambios
+                  components={animatedComponents}
+                  classNamePrefix="select"
+                  placeholder="Choose options..." // Placeholder para las opciones
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col my-2 mn:flex-row w-full">
+              <div className="range1 flex flex-col w-full">
+                <label className="w-full flex flex-row text-start">Ph:</label>
+                <Slider
+                  min={0.1}
+                  max={10.0}
+                  step={0.1}
+                  className="w-full"
+                  value={sliderValue3}
+                  onChange={(value) => setSliderValue3(value as number)}
+                  trackStyle={{ backgroundColor: "blue", height: 10 }}
+                  handleStyle={{
+                    borderColor: "blue",
+                    height: 20,
+                    width: 20,
+                    marginTop: -5,
+                    backgroundColor: "white",
+                  }}
+                  railStyle={{ backgroundColor: "gray", height: 10 }}
+                />
+                <p className="flex w-full my-2">
+                  Selected Value: {sliderValue3}
+                </p>
+              </div>
+            </div>
+
             <button
               onClick={hidePopUp}
               className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
